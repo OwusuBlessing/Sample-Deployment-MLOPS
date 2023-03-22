cdc637bab92e02f59f0630e78ccfb4df22a5b339
OwusuBlessing <owususammy509@gmail.com>
2023-03-22 20:47:51 +0100

    notebooks files added

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,9 @@
 import logging
 import sys #used to manipulate different part of python enviroment
-import logging
+#import logging
+import logger
+from logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -17,11 +20,12 @@ class CustomException(Exception):
     def  __str__(self):
         return self.error_mesaage
 
-'''if __name__=="__main__":
+
+if __name__=="__main__":
     try:
         a = 1/0
     except Exception as e:
-        logging.info("Logging has started")
-        raise CustomException(e,sys)'''
+        logging.info("Division by zero")
+        raise CustomException(e,sys)
         
         
